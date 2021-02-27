00b597ecf9166bc06734dcad66848cbc6cda91b4
Suzanne Soy <ligo@suzanne.soy>
2021-02-27 00:00:47 +0000

    Fixed some paths (typo)

--- parse/pre.rkt
+++ parse/pre.rkt
@@ -5,10 +5,10 @@
            stxparse-info/my-include)
 (version-case
   [(version< (version) "6.11.0.900")
-    (my-include "../6-11/stxparse-info/parse/pre.rkt")]
+    (my-include "../6-11/racket/collects/syntax/parse/pre.rkt")]
   [(version< (version) "6.90.0.29")
-    (my-include "../6-12/stxparse-info/parse/pre.rkt")]
+    (my-include "../6-12/racket/collects/syntax/parse/pre.rkt")]
   [(version< (version) "7.0.0.20")
-    (my-include "../6-90-0-29/stxparse-info/parse/pre.rkt")]
+    (my-include "../6-90-0-29/racket/collects/syntax/parse/pre.rkt")]
   [else
-    (my-include "../7-0-0-20/stxparse-info/parse/pre.rkt")])
+    (my-include "../7-0-0-20/racket/collects/syntax/parse/pre.rkt")])
