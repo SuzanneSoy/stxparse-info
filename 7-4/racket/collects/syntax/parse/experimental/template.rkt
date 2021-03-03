d64d2c1901bc0e369008511e3abc40f089450830
Suzanne Soy <ligo@suzanne.soy>
2021-03-03 03:21:48 +0000

    Upgrade for Racket 7.4

---
+++ 7-4/racket/collects/syntax/parse/experimental/template.rkt
@@ -0,0 +1,49 @@
+#lang racket/base
+(require (for-syntax racket/base
+                     auto-syntax-e/utils)
+         (only-in racket/private/template
+                  metafunction))
+(provide (rename-out [syntax template]
+                     [syntax/loc template/loc]
+                     [quasisyntax quasitemplate]
+                     [quasisyntax/loc quasitemplate/loc]
+                     [~? ??]
+                     [~@ ?@])
+         define-template-metafunction)
+
+;; ============================================================
+;; Metafunctions
+
+(define-syntax (define-template-metafunction stx)
+  (syntax-case stx ()
+    [(dsm (id arg ...) . body)
+     #'(dsm id (lambda (arg ...) . body))]
+    [(dsm id expr)
+     (identifier? #'id)
+     (with-syntax ([(internal-id) (generate-temporaries #'(id))])
+       #'(begin (define internal-id (make-hygienic-metafunction expr))
+                (define-syntax id (metafunction (quote-syntax internal-id)))))]))
+
+(define current-template-metafunction-introducer
+  (make-parameter (lambda (stx) (if (syntax-transforming?) (syntax-local-introduce stx) stx))))
+
+(define old-template-metafunction-introducer
+  (make-parameter #f))
+
+(define (syntax-local-template-metafunction-introduce stx)
+  (let ([mark (current-template-metafunction-introducer)]
+        [old-mark (old-template-metafunction-introducer)])
+    (unless old-mark
+      (error 'syntax-local-template-metafunction-introduce
+             "must be called within the dynamic extent of a template metafunction"))
+    (mark (old-mark stx))))
+
+(define ((make-hygienic-metafunction transformer) stx)
+  (define mark (make-syntax-introducer))
+  (define old-mark (current-template-metafunction-introducer))
+  (parameterize ((current-template-metafunction-introducer mark)
+                 (old-template-metafunction-introducer old-mark))
+    (define r (call-with-continuation-barrier (lambda () (transformer (mark (old-mark stx))))))
+    (unless (syntax? r)
+      (raise-syntax-error #f "result of template metafunction was not syntax" stx))
+    (old-mark (mark r))))
--- info.rkt
+++ info.rkt
@@ -11,8 +11,8 @@
                      "racket-doc"
                      "at-exp-lib")) ;; for the documentation only
 (define scribblings '(("scribblings/stxparse-info.scrbl" () ("Syntax Extensions"))))
-(define compile-omit-paths '("6-11" "6-12" "6-90-0-29" "7-0-0-20" "7-3-0-1" "8-0"))
-(define test-omit-paths '("6-11" "6-12" "6-90-0-29" "7-0-0-20" "7-3-0-1" "8-0"))
+(define compile-omit-paths '("6-11" "6-12" "6-90-0-29" "7-0-0-20" "7-3-0-1" "7-4" "8-0"))
+(define test-omit-paths '("6-11" "6-12" "6-90-0-29" "7-0-0-20" "7-3-0-1" "7-4" "8-0"))
 (define pkg-desc "Description Here")
 (define version "0.0")
 (define pkg-authors '(Suzanne Soy))
